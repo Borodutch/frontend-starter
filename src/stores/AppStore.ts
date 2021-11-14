824aadade4780bc48ec7d9521248a794dfad5df3
backmeupplz <backmeupplz@gmail.com>
2021-11-14 10:16:32 -0800

    fix spacing

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -1,6 +1,7 @@
 import { proxy } from 'valtio'
 import Language from 'models/Language'
 import PersistableStore from 'stores/persistence/PersistableStore'
+
 class AppStore extends PersistableStore {
   language: Language = Language.en
 }
