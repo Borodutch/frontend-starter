aed6e8bb9aeb49b4a186e1f216a66f38862148c7
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-11-23 10:29:34 -0800

    add linter rule

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import './index.css'
+import 'index.css'
 import { render } from 'preact'
-import App from './App'
+import App from 'App'
 
 render(<App />, document.getElementById('root') as Element)
