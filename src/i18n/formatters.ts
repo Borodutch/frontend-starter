8a9e270c43133cdf2133405f5bbda149bd22bf8f
Ruslan Zapevalov <upacyxou@tutanota.com>
2021-12-15 02:26:07 +0400

    fix eslint for autogenerated file

--- src/i18n/formatters.ts
+++ src/i18n/formatters.ts
@@ -1,3 +1,4 @@
+/* eslint-disable require-await */
 /* eslint-disable import/prefer-default-export */
 import type { AsyncFormattersInitializer } from 'typesafe-i18n'
 import type { Formatters, Locales } from 'i18n/i18n-types'
@@ -5,7 +6,7 @@ import type { Formatters, Locales } from 'i18n/i18n-types'
 export const initFormatters: AsyncFormattersInitializer<
   Locales,
   Formatters
-  // eslint-disable-next-line require-await
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
 > = async (locale: Locales) => {
   const formatters: Formatters = {
     // add your formatter functions here
