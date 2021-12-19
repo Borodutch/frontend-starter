efb8cb2d611b6137b15f780af7d63464b47c0a9d
Ruslan Zapevalov <upacyxou@tutanota.com>
2021-12-19 21:45:41 +0400

    upgrade to tailwind 3

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,16 +1,22 @@
 import { FC } from 'react'
-import { classnames } from 'classnames/tailwind'
+import {
+  classnames,
+  fontSize,
+  fontWeight,
+  margin,
+  textAlign,
+  textColor,
+} from 'classnames/tailwind'
 
-const whiteText = classnames('text-white')
-const grayText = classnames('text-gray-300')
+const whiteText = classnames(textColor('text-white'))
+const grayText = classnames(textColor('text-gray-300'))
 
 const headerText = classnames(
   whiteText,
-  'text-3xl',
-  'md:text-6xl',
-  'font-bold',
-  'text-center',
-  'mb-6'
+  fontSize('text-3xl', 'md:text-6xl'),
+  fontWeight('font-bold'),
+  textAlign('text-center'),
+  margin('mb-6')
 )
 export const HeaderText: FC = ({ children }) => {
   return <p className={headerText}>{children}</p>
@@ -18,17 +24,16 @@ export const HeaderText: FC = ({ children }) => {
 
 const subheaderText = classnames(
   whiteText,
-  'text-xl',
-  'md:text-2xl',
-  'font-bold',
-  'text-center',
-  'my-12'
+  fontSize('text-xl', 'md:text-2xl'),
+  fontWeight('font-bold'),
+  textAlign('text-center'),
+  margin('my-12')
 )
 export const SubheaderText: FC = ({ children }) => {
   return <p className={subheaderText}>{children}</p>
 }
 
-const bodyText = classnames(grayText, 'text-center')
+const bodyText = classnames(grayText, textAlign('text-center'))
 export const BodyText: FC = ({ children }) => {
   return <p className={bodyText}>{children}</p>
 }
