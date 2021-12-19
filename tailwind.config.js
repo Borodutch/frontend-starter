04f75121884606b5d715b15cd91f15b24f63d313
Ruslan Zapevalov <upacyxou@tutanota.com>
2021-12-19 22:22:09 +0400

    fix background & upgrade autoperfix to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  purge: ['./public/index.html', './src/**/!(tailwind).{ts,tsx}'],
+  content: ['./public/index.html', './src/**/!(tailwind).{ts,tsx}'],
   theme: {
     extend: {
       colors: {
